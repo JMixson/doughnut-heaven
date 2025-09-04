8a0ab63f5bd5c32d38d7b780562d90d2081b2b12
Jasmine Mixson <jmixson1179@gmail.com>
2025-09-03 22:56:36 -0700

    Create and add main navigation

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,14 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Navbar from './components/Navbar.vue';
+</script>
 
 <template>
+  <Navbar />
   <h1>You did it!</h1>
   <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
+    Visit
+    <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to
+    read the documentation
   </p>
 </template>
 
---
+++ src/components/Navbar.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts"></script>
+
+<template>
+  <nav role="navigation">
+    <a href="#">DH</a>
+
+    <ul>
+      <li><a href="#love">Why the Love?</a></li>
+      <li><a href="#types">Types</a></li>
+      <li><a href="#facts">Fun Facts</a></li>
+      <li><a href="#credits">Credits</a></li>
+    </ul>
+  </nav>
+</template>
+
+<style scoped lang="css">
+nav {
+  display: flex;
+  justify-content: space-between;
+  font-family: var(--special-font);
+  padding: 20px var(--padding-sides);
+  font-size: var(--md-font-size);
+}
+
+ul {
+  display: flex;
+  list-style: none;
+
+  li {
+    display: inline-block;
+    margin-inline-start: 15px;
+  }
+}
+
+a {
+  &:link,
+  &:visited {
+    text-decoration: none;
+    color: var(--color-black);
+  }
+
+  &:hover,
+  &:active {
+    text-decoration: underline;
+  }
+}
+</style>
